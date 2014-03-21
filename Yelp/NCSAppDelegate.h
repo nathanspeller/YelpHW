14b4d3fbc95ad681811d0df0b4c83cf2f25df5d7
nathanspeller <speller.nathan@gmail.com>
2014-03-20 17:34:15 -0700

    Initial Commit

---
+++ Yelp/NCSAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  NCSAppDelegate.h
+//  Yelp
+//
+//  Created by Nathan Speller on 3/20/14.
+//  Copyright (c) 2014 Nathan Speller. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface NCSAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Yelp/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Yelp
+//
+//  Created by Nathan Speller on 3/20/14.
+//  Copyright (c) 2014 Nathan Speller. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "NCSAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([NCSAppDelegate class]));
+    }
+}
